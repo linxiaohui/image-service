01f49b09cb449659fa63305db88765f31005fa2d
linxiaohui <llinxiaohui@126.com>
2020-09-18 15:14:03 +0800

    U-2-Net AI抠图的实现

--- U-2-Net/u2net_rpc.py
+++ U-2-Net/u2net_rpc.py
@@ -1,8 +1,10 @@
 #基于U-2-Net项目的u2net_test.py文件修改
 import os
+import io
 import glob
+import time
 
-from skimage import io, transform
+import skimage
 import torch
 import torchvision
 from torch.autograd import Variable
@@ -39,20 +41,17 @@ def gen_output(image_name,pred):
     predict_np = predict.cpu().data.numpy()
     im = Image.fromarray(predict_np*255).convert('RGB')
     img_name = image_name.split(os.sep)[-1]
-    image = io.imread(image_name)
+    image = skimage.io.imread(image_name)
     imo = im.resize((image.shape[1],image.shape[0]),resample=Image.BILINEAR)
-    pb_np = np.array(imo)
-    aaa = img_name.split(".")
-    bbb = aaa[0:-1]
-    imidx = bbb[0]
-    for i in range(1,len(bbb)):
-        imidx = imidx + "." + bbb[i]
-    imo.save(d_dir+imidx+'.png')
+    b = io.BytesIO()
+    imo.save(b, "png")
+    data = b.getvalue()
+    return data
 
 
 
 MODEL_NAME = 'u2net' #或者选择 u2netp
-MODEL_DIR = os.path.join(os.getcwd(), 'models', MODEL_NAME + '.pth')
+MODEL_DIR = os.path.join(os.getcwd(), 'model', MODEL_NAME + '.pth')
 
 if(MODEL_NAME == 'u2net'):
     print("...load U2NET---173.6 MB")
@@ -60,13 +59,14 @@ if(MODEL_NAME == 'u2net'):
 elif(MODEL_NAME == 'u2netp'):
     print("...load U2NEP---4.7 MB")
     NET = U2NETP(3,1)
-NET.load_state_dict(torch.load(MODEL_DIR))
+#NET.load_state_dict(torch.load(MODEL_DIR,  map_location=lambda storage, loc: storage))
+NET.load_state_dict(torch.load(MODEL_DIR,  map_location='cpu'))
 if torch.cuda.is_available():
     NET.cuda()
 NET.eval()
 
 
-def cutout(image_data, ext='jpg'):
+def image_cutout(image_data, ext='jpg'):
     if isinstance(image_data, list):
         image_data_list = image_data
     else:
@@ -77,7 +77,7 @@ def cutout(image_data, ext='jpg'):
         fn = "{}.{}".format(time.time(), ext)
         with open(fn, "wb") as fp:
             fp.write(image_data)
-        image_data_list.append(fn)
+        img_name_list.append(fn)
 
     result_list = []    
     test_salobj_dataset = SalObjDataset(img_name_list = img_name_list,
@@ -103,14 +103,21 @@ def cutout(image_data, ext='jpg'):
         pred = d1[:,0,:,:]
         pred = normPRED(pred)
 
-        # save results to test_results folder
-        if not os.path.exists(prediction_dir):
-            os.makedirs(prediction_dir, exist_ok=True)
-        save_output(img_name_list[i_test],pred,prediction_dir)
-
+        dat = gen_output(img_name_list[i_test],pred)
+        result_list.append(dat)
         del d1,d2,d3,d4,d5,d6,d7
 
     for fn in img_name_list:
         os.remove(fn)
 
     return result_list
+
+class U2NetCutOut(object):
+    def cutout(self, image_data):
+        return image_cutout(image_data)
+
+
+s = zerorpc.Server(U2NetCutOut())
+s.bind("tcp://0.0.0.0:54323")
+s.run()
+
