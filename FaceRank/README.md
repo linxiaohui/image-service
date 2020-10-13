fa1787f3442bb2b947c133646f31ba6dc1de510c
linxiaohui <llinxiaohui@126.com>
2020-10-13 17:04:29 +0800

    FaceRank

---
+++ FaceRank/README.md
@@ -0,0 +1,13 @@
+# 主页
+[项目的github地址](https://github.com/pwfee/FaceRank)
+
+该项目使用dlib进行人脸检测，利用公式计算“颜值”；
+
+# 相关
+   * [FaceDetect](../FaceDetect) 使用OpenCV进行人脸的检测
+   * [BeautyPredict](../BeautyPredict) 使用深度学习进行模型的“颜值评分”
+   * [Baidu AIP人脸API](https://cloud.baidu.com/doc/FACE/s/ek37c1qiz)
+   * [Face++ API](https://console.faceplusplus.com.cn/documents/4888373)
+
+更多信息可以参见上述项目的主页.
+
---
+++ FaceRank/face_rank.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import io
+import time
+
+import zerorpc
+import cv2
+import dlib
+
+from dlib_landmarks import *
+
+DETECTOR = dlib.get_frontal_face_detector()
+LANDMARKER = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+def infer_image_type(image_data):
+    """根据图片的内容推断图片的格式"""
+    if image_data[:8] == b'\x89PNG\r\n\x1a\n':
+        return ".png"
+    if image_data[:2] == b'\xff\xd8':
+        return ".jpg"
+    return ".jpg"
+
+class Point2D(object):
+  def __init__(self):
+    self.x = 0
+    self.y = 0
+
+def cacl_distance(p1, p2):
+  return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2))
+
+
+def cacl_mid_point(p1, p2):
+  res_mid_point = Point2D()
+  res_mid_point.x = (p1.x + p2.x) / 2
+  res_mid_point.y = (p1.y + p2.y) / 2
+  return res_mid_point
+
+def facial_rater(shape):
+    eyebrow_mid = cacl_mid_point(shape.part(left_eyebrow_right_corner),
+                                 shape.part(right_eyebrow_left_corner))
+    d1 = cacl_distance(shape.part(nose_contour_lower_middle), eyebrow_mid)
+    d2 = cacl_distance(shape.part(left_eye_right_corner),
+                       shape.part(right_eye_left_corner))
+    d3 = cacl_distance(shape.part(nose_left), shape.part(nose_right))
+    d4 = cacl_distance(shape.part(contour_left1), shape.part(contour_right1))
+    d5 = cacl_distance(shape.part(contour_chin),
+                       shape.part(nose_contour_lower_middle))
+    d6_left = cacl_distance(shape.part(left_eye_left_corner),
+                            shape.part(left_eye_right_corner))
+    d6_right = cacl_distance(shape.part(right_eye_left_corner),
+                             shape.part(right_eye_right_corner))
+    d7 = cacl_distance(shape.part(mouth_left_corner),
+                       shape.part(mouth_right_corner))
+    d8 = cacl_distance(shape.part(contour_left4), shape.part(contour_right4))
+
+    init_score = 100
+    deduction = 0
+
+    # 眼角距离为脸宽的 1/5，
+    deduction += fabs((d2 / d4) * 100 - 25)
+    # 鼻子宽度为脸宽的 1/5
+    deduction += fabs((d3 / d4) * 100 - 25)
+    # 眼睛的宽度，应为同一水平脸部宽度的 1/5
+    eye_dist_avg = (d6_left + d6_right) / 2
+    deduction += fabs(eye_dist_avg / d4 * 100 - 25)
+    # 理想嘴巴宽度应为同一脸部宽度的 1/2
+    deduction += fabs((d7 / d8) * 100 - 50)
+    # 下巴到鼻子下方的高度 == 眉毛中点到鼻子最低处的距离
+    deduction += fabs(d5 - d1)
+    final_score = init_score - deduction + 20
+    return final_score
+
+def facial_processor(img, shape, image_type='png'):
+    for i in range(68):
+      cv2.circle(img, (shape.part(i).x, shape.part(i).y), 5, (0, 255, 0), -1, 8)
+    is_success, im_buf_arr = cv2.imencode(image_type, im)
+    byte_im = im_buf_arr.tobytes()
+    return byte_im
+
+    
+def face_detector(image_data):
+    """检测图片数据中的人脸，返回一个元组；
+    如果找到则标记其中第一张脸的位置及评分，否则返回(None, None)"""
+    ext = infer_image_type(image_data)
+    fn = "{}.{}".format(time.time(), ext)
+    with open(fn, "wb") as fp:
+        fp.write(image_data)
+    img = cv2.imread(fn)
+    faces = detector(img, 1)
+    if len(faces)<=0:
+        return None, None
+    else:
+        shape = LANDMARKER(img, faces[0])
+        score = facial_rater(shape)
+        marked_img = facial_processor(img, shape)
+        return marked_img, score
+    
+
+class FaceScorer(object):
+    def face_score(self, image_data):
+        return face_detector(image_data)
+
+
+s = zerorpc.Server(ChangeBackGroundColor())
+s.bind("tcp://0.0.0.0:54327")
+s.run()
+
+
