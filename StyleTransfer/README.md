b18df882de9321f3d1a64b2aad78721b40b69af0
linxiaohui <llinxiaohui@126.com>
2020-10-21 08:20:08 +0800

    StyleTransfer RPC

---
+++ StyleTransfer/README.md
@@ -0,0 +1,65 @@
+# 简介
+
+本项目使用[fast-neural-style](https://github.com/jcjohnson/fast-neural-style)提供的风格迁移的预训练模型，使用OpenCV的DNN模块加载预训练模型。 OpenCV不支持模型的训练，只支持模型推理；已经支持TensorFlow、Pytorch/Torch、Caffe、DarkNet等模型的读取。
+
+# 模型地址
+
+## 从官方地址下载的脚本
+```bash
+BASE_URL="https://cs.stanford.edu/people/jcjohns/fast-neural-style/models/"
+
+mkdir -p models/instance_norm
+cd models/instance_norm
+curl -O "$BASE_URL/instance_norm/candy.t7"
+curl -O "$BASE_URL/instance_norm/la_muse.t7"
+curl -O "$BASE_URL/instance_norm/mosaic.t7"
+curl -O "$BASE_URL/instance_norm/feathers.t7"
+curl -O "$BASE_URL/instance_norm/the_scream.t7"
+curl -O "$BASE_URL/instance_norm/udnie.t7"
+
+mkdir -p ../eccv16
+cd ../eccv16
+curl -O "$BASE_URL/eccv16/the_wave.t7"
+curl -O "$BASE_URL/eccv16/starry_night.t7"
+curl -O "$BASE_URL/eccv16/la_muse.t7"
+curl -O "$BASE_URL/eccv16/composition_vii.t7"
+cd ../../
+```
+
+## 其他地址
+[百度云]()
+```
+SHA256(models/eccv16/composition_vii.t7)= 34b66175dffa75d47300ca9f569e4da98face3bde3c23b3954ca5cb055e64904
+SHA256(models/eccv16/la_muse.t7)= 4b821d3b4d70a42bb5caee82e1ae45a7e3d85f42c9256adfa5c9bb296e881115
+SHA256(models/eccv16/starry_night.t7)= f80aedd413c5ba913e94d4812cbca73bd6c073f10c26e78ca58ee9ed5f34821b
+SHA256(models/eccv16/the_wave.t7)= 57fb102191b19d1fea34df843fc4c290166a6a594fff2607e0b93459c473a9fb
+SHA256(models/instance_norm/candy.t7)= 5bff84714d55b35a646822ab7199b6d40fa5fd60a8560f8f1709500b4cd8f1c4
+SHA256(models/instance_norm/feathers.t7)= 51c0c48700579541d51bdbd0907de9767858dacf0e3e84e5c127335236f8091e
+SHA256(models/instance_norm/la_muse.t7)= 6e22676a5565e30735f6b8ad062ffc2ae67ca471732213237ebfd559f12fc428
+SHA256(models/instance_norm/mosaic.t7)= fbd7d882e9e02aafb57366e726762025ff6b2e12cd41abd44b874542b7693771
+SHA256(models/instance_norm/the_scream.t7)= b8443f5573433f690cffafff55f441ac28a14c053179b0c33ddcf4da670f3732
+SHA256(models/instance_norm/udnie.t7)= 3a3a2cf3472085f26d8e4bc26a1e6b2eb48e6dfc4df8ec9a9b1b0d4085aefa36
+```
+
+# 构建
+   1. 在当前目录执行下载预训练模型的脚本，下载模型文件；至`models`文件中
+   2. `docker build -t style-transfer:1.0 .`
+
+# dockerhub
+   `docker run -d -p 54330:54330 linxiaohui/style-transfer:1.0`
+
+# 调用方式
+
+```python
+# -*- coding: utf-8 -*-
+import zerorpc
+s = zerorpc.Client(heartbeat=None, timeout=180)
+s.connect("tcp://127.0.0.1:54330")
+
+data_path = "x.jpg"
+data = open(data_path, "rb").read()
+img = s.style_transfer(data, "udnie")
+dat = img
+with open("r.jpg", "wb") as fp:
+    fp.write(dat)
+```
--- StyleTransfer/style-transfer.py
+++ StyleTransfer/style-transfer.py
@@ -1,54 +1,67 @@
+# -*- coding:utf-8 -*-
+"""
+参考 https://www.cvpy.net/studio/cv/func/DeepLearning/style_transfer/style_transfer/page/ 中的介绍，
+使用OpenCV加载预训练模型，进行图片风格迁移
+"""
+import os
+import imghdr
+
 import cv2
+import numpy as np
+import zerorpc
 
-model_base_dir = "./models/instance_norm/"
-d_model_map = {
-    1: "udnie",
-    2: "la_muse",
-    3: "the_scream",
-    4: "candy",
-    5: "mosaic",
-    6: "feathers",
-    7: "starry_night"
-}
+MODELS_PATH = os.path.join(os.environ['IMAGESERVICE_ROOT'], "models")
 
-def get_model_from_style(style: int):
+STYLE_TRANS_MODLES = {}
+
+def get_model_from_style(style):
     """
     加载指定风格的模型
-    :param style: 模型编码
+    :param style: 模型的名称
     :return: model
     """
-    model_name = d_model_map.get(style, "mosaic")
-    model_path = model_base_dir + model_name + ".t7"
-    print(model_path)
+    if style in STYLE_TRANS_MODLES:
+        return STYLE_TRANS_MODLES[style]
+    if style in ["candy", "feathers", "la_muse",  "mosaic", "the_scream", "udnie"]:
+        SUBPATH = "instance_norm"
+    else:
+        SUBPATH = "eccv16"
+    model_path = os.path.join(MODELS_PATH, SUBPATH, style+".t7")
     model = cv2.dnn.readNetFromTorch(model_path)
+    model = setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+    STYLE_TRANS_MODLES[style] = model
     return model
 
 net = get_model_from_style(3)
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 
-img = cv2.imread("x.jpg")
 
-print("model read")
+def transfer_stype(image_data, style):
+    ext = imghdr.what(None, image_data)
+    im = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)
+    (h, w) = img.shape[:2]
+    blob = cv2.dnn.blobFromImage(img, 1.0, (w, h), (103.939, 116.779, 123.680), swapRB=False, crop=False)
+    net = get_model_from_style(style)
+    net.setInput(blob)
+    output = net.forward()
+    # reshape输出结果, 将减去的平均值加回来，并交换各颜色通道
+    output = output.reshape((3, output.shape[2], output.shape[3]))
+    output[0] += 103.939
+    output[1] += 116.779
+    output[2] += 123.680
+    output = output.transpose(1, 2, 0)
+    is_success, im_buf_arr = cv2.imencode(ext, im)
+    byte_im = im_buf_arr.tobytes()
 
-(h, w) = img.shape[:2]
-blob = cv2.dnn.blobFromImage(img, 1.0, (w, h), (103.939, 116.779, 123.680), swapRB=False, crop=False)
 
-print("blob")
 
-net.setInput(blob)
+class StyleTransfer(object):
+    def style_transfer(self, image_data):
+        return transfer_stype(image_data)
 
-print("net input")
 
-output = net.forward()
-
-print("forward")
-
-# reshape输出结果, 将减去的平均值加回来，并交换各颜色通道
-output = output.reshape((3, output.shape[2], output.shape[3]))
-output[0] += 103.939
-output[1] += 116.779
-output[2] += 123.680
-output = output.transpose(1, 2, 0)
-
-cv2.imwrite("result.jpg", output)
+if __name__ == "__main__":
+    s = zerorpc.Server(StyleTransfer())
+    s.bind("tcp://0.0.0.0:54330")
+    s.run()
 
