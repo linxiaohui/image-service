1a6864cf9ed60a22b6c99fc4a43272970893112f
linxiaohui <llinxiaohui@126.com>
2020-10-26 13:21:55 +0800

    RPC

--- DeepStyle/deep_style.py
+++ DeepStyle/deep_style.py
@@ -6,30 +6,39 @@ import os
 import sys
 import imghdr
 
+import zerorpc
 import numpy as np
 import cv2
 
 from cores import Opt
 from models import loadmodel, runmodel
 
-def style_transfer(image_data, style):
-    """
-    style:
-        apple2orange
-        orange2apple
-        summer2winter
-        winter2summer.pth'
-        cezanne
-        monet
-        ukiyoe
-        vangogh
-    """
-    image_type = imghdr.what(None, image_data)
-    opt = Opt()
-    opt.model_path = os.path.join("pretrained_models", f"style_{style}.pth")
-    netG = loadmodel.style(opt)
-    img = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)
-    img = runmodel.run_styletransfer(opt, netG, img)
-    is_success, im_buf_arr = cv2.imencode(image_type, img)
-    byte_im = im_buf_arr.tobytes()
-    return byte_im
+
+class DeepMosaic_Style(object):
+    def style_transfer(self, image_data, style):
+        """
+        style:
+            apple2orange
+            orange2apple
+            summer2winter
+            winter2summer.pth'
+            cezanne
+            monet
+            ukiyoe
+            vangogh
+        """
+        image_type = imghdr.what(None, image_data)
+        opt = Opt()
+        opt.model_path = os.path.join("pretrained_models", f"style_{style}.pth")
+        netG = loadmodel.style(opt)
+        img = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)
+        img = runmodel.run_styletransfer(opt, netG, img)
+        is_success, im_buf_arr = cv2.imencode(image_type, img)
+        byte_im = im_buf_arr.tobytes()
+        return byte_im
+
+if __name__ == "__main__":
+    s = zerorpc.Server(DeepMosaic_Style())
+    s.bind("tcp://0.0.0.0:54332")
+    s.run()
+
