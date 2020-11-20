cc19c924ea300bd6910fc61a3ad76f52e0e02451
linxiaohui <llinxiaohui@126.com>
2020-11-20 08:35:21 +0800

    fix #14

--- CertPhoto/chg_bg.py
+++ CertPhoto/chg_bg.py
@@ -2,8 +2,8 @@
 # 基于U-2-Net项目的u2net_test.py文件修改
 import os
 import io
-import glob
 import time
+import imghdr
 
 import skimage
 import torch
@@ -35,14 +35,14 @@ def normPRED(d):
     dn = (d-mi)/(ma-mi)
     return dn
 
-def gen_mask(image_name,pred):
+def gen_mask(image_name, pred):
     predict = pred
     predict = predict.squeeze()
     predict_np = predict.cpu().data.numpy()
     im = Image.fromarray(predict_np*255).convert('RGB')
     img_name = image_name.split(os.sep)[-1]
     image = skimage.io.imread(image_name)
-    imo = im.resize((image.shape[1],image.shape[0]),resample=Image.BILINEAR)
+    imo = im.resize((image.shape[1], image.shape[0]), resample=Image.BILINEAR)
     b = io.BytesIO()
     imo.save(b, "png")
     data = b.getvalue()
@@ -60,20 +60,20 @@ def gen_output(image_name, pred, bg_color):
     # image 的类型为 numpy.ndarray， shape=(height,width, channel)
     ori = Image.open(image_name)
     # ori的类型为 PIL.JpegImagePlugin.JpegImageFile， size=(width, height)
-    imo = im.resize((image.shape[1],image.shape[0]),resample=Image.BILINEAR)
+    imo = im.resize((image.shape[1], image.shape[0]), resample=Image.BILINEAR)
     mask = np.asarray(imo)
     # mask 类型为 numpy.ndarray，元素为 np.uint8, shape=(height,width, channel)
-    result = np.zeros((image.shape[0],image.shape[1],4),dtype=np.uint8)
-    result[:,:,0]=image[:,:,0]
-    result[:,:,1]=image[:,:,1]
-    result[:,:,2]=image[:,:,2]
-    result[:,:,3]=mask[:,:,0]
+    result = np.zeros((image.shape[0], image.shape[1], 4), dtype=np.uint8)
+    result[:, :, 0] = image[:, :, 0]
+    result[:, :, 1] = image[:, :, 1]
+    result[:, :, 2] = image[:, :, 2]
+    result[:, :, 3] = mask[:, :, 0]
     imo = Image.fromarray(np.uint8(result)).convert("RGBA")
     # 修改背景颜色
     bg_color_code = ImageColor.getrgb(bg_color)
     print("debug", bg_color, bg_color_code)
     bg = Image.new('RGBA', imo.size, bg_color_code)
-    x,y = imo.size
+    x, y = imo.size
     bg.paste(imo, (0, 0, x, y), imo)
     b = io.BytesIO()
     bg.save(b, "png")
@@ -83,20 +83,12 @@ def gen_output(image_name, pred, bg_color):
 
 MODEL_DIR = os.path.join(os.environ['IMAGESERVICE_ROOT'], 'models', 'u2net.pth')
 print("...load U2NET---173.6 MB")
-NET = U2NET(3,1)
-#NET.load_state_dict(torch.load(MODEL_DIR,  map_location=lambda storage, loc: storage))
+NET = U2NET(3, 1)
+# NET.load_state_dict(torch.load(MODEL_DIR,  map_location=lambda storage, loc: storage))
 NET.load_state_dict(torch.load(MODEL_DIR,  map_location='cpu'))
 if torch.cuda.is_available():
     NET.cuda()
 NET.eval()
-
-def infer_image_type(image_data):
-    """根据图片的内容推断图片的格式"""
-    if image_data[:8] == b'\x89PNG\r\n\x1a\n':
-        return ".png"
-    if image_data[:2] == b'\xff\xd8':
-        return ".jpg"
-    return ".jpg"
 
 def change_background(image_data, bg_color='white'):
     print("change_background color=", bg_color)
@@ -106,17 +98,16 @@ def change_background(image_data, bg_color='white'):
         image_data_list = [image_data]
     img_name_list = []
     for image_data in image_data_list:
-        ext = infer_image_type(image_data)
+        ext = imghdr.what(None, image_data)
         fn = "{}.{}".format(time.time(), ext)
         with open(fn, "wb") as fp:
             fp.write(image_data)
         img_name_list.append(fn)
 
     result_list = []    
-    test_salobj_dataset = SalObjDataset(img_name_list = img_name_list,
-                                        lbl_name_list = [],
-                                        transform=transforms.Compose([RescaleT(320),
-                                                                        ToTensorLab(flag=0)])
+    test_salobj_dataset = SalObjDataset(img_name_list=img_name_list,
+                                        lbl_name_list=[],
+                                        transform=transforms.Compose([RescaleT(320), ToTensorLab(flag=0)])
                                         )
     test_salobj_dataloader = DataLoader(test_salobj_dataset,
                                         batch_size=1,
@@ -131,14 +122,13 @@ def change_background(image_data, bg_color='white'):
         else:
             inputs_test = Variable(inputs_test)
 
-        d1,d2,d3,d4,d5,d6,d7 = NET(inputs_test)
+        d1, d2, d3, d4, d5, d6, d7 = NET(inputs_test)
         # normalization
-        pred = d1[:,0,:,:]
+        pred = d1[:, 0, :, :]
         pred = normPRED(pred)
-
         dat = gen_output(img_name_list[i_test], pred, bg_color)
         result_list.append(dat)
-        del d1,d2,d3,d4,d5,d6,d7
+        del d1, d2, d3, d4, d5, d6, d7
 
     for fn in img_name_list:
         os.remove(fn)
