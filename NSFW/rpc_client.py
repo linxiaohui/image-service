34d708229341772ba7daeb465281b80637a214d3
linxiaohui <llinxiaohui@126.com>
2020-11-24 08:40:29 +0800

    NSFW 示例代码更新

--- NSFW/rpc_client.py
+++ NSFW/rpc_client.py
@@ -5,10 +5,17 @@ import zerorpc
 s = zerorpc.Client(heartbeat=None, timeout=60)
 s.connect("tcp://127.0.0.1:54322")
 
-
-data_path = "/home/linxh/data/BeautyPredict/samples/image/test7.jpg"
-
+# 单张图片
+data_path = "1.jpg"
 data = open(data_path, "rb").read()
-
 print(s.nsfw_predict(data))
+
+# 批量调用
+images = []
+for i, n in enumerate(["1.jpg", "2.jpg"]):
+    with open(n, "rb") as fp:
+        images.append((i, fp.read()))
+print(s.nsfw_batch_predict(images))
+
+
 
