72b1d7c141e0dc55614633df487e87ebab0b7458
linxiaohui <llinxiaohui@126.com>
2020-11-25 08:52:17 +0800

    FaceDetect fix #14

--- FaceDetect/face_decet_rpc.py
+++ FaceDetect/face_decet_rpc.py
@@ -2,6 +2,7 @@
 import os
 import io
 import time
+import imghdr
 
 import numpy as np
 import cv2
@@ -14,7 +15,8 @@ class FaceDetector(object):
         )
         self.factor = 1
 
-    def face_mark(self, image_data, ext="jpg"):
+    def face_mark(self, image_data):
+        ext = imghdr.what(None, image_data)
         fn = "{}.{}".format(time.time(), ext)
         with open(fn, "wb") as fp:
             fp.write(image_data)
@@ -22,17 +24,13 @@ class FaceDetector(object):
         img = cv2.resize(img, None, fx=self.factor, fy=self.factor, interpolation=cv2.INTER_CUBIC)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
-        for (x,y,w,h) in faces:
-            img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        for (x, y, w, h) in faces:
+            img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
         is_success, im_buf_arr = cv2.imencode("."+ext, img)
         byte_im = im_buf_arr.tobytes()
         return byte_im
 
-    
-
-    
 if __name__ == "__main__":
     s = zerorpc.Server(FaceDetector())
     s.bind("tcp://0.0.0.0:54324")
     s.run()
-
