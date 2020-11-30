c97c8068d495dd3e23c37ebcabf329cd8ce6e42b
linxiaohui <llinxiaohui@126.com>
2020-11-30 08:33:27 +0800

    fix #14

--- FaceSketch/face_sketch.py
+++ FaceSketch/face_sketch.py
@@ -2,6 +2,7 @@
 import os
 import io
 import time
+import imghdr
 
 import skimage
 import torch
@@ -10,12 +11,13 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms#, utils
+from torchvision import transforms
 # import torch.optim as optim
 
 import zerorpc
 import numpy as np
 from PIL import Image
+from PIL import ImageOps
 
 from data_loader import RescaleT
 from data_loader import ToTensor
@@ -40,6 +42,7 @@ def gen_output(image_name,pred):
     img_name = image_name.split(os.sep)[-1]
     image = skimage.io.imread(image_name)
     imo = im.resize((image.shape[1],image.shape[0]),resample=Image.BILINEAR)
+    imo = ImageOps.invert(imo)
     b = io.BytesIO()
     imo.save(b, "png")
     data = b.getvalue()
@@ -48,19 +51,11 @@ def gen_output(image_name,pred):
 MODEL_DIR = os.path.join(os.environ['IMAGESERVICE_ROOT'], 'models', 'sketch.pth')
 print("...load sketch---173.6 MB")
 NET = U2NET(3,1)
-#NET.load_state_dict(torch.load(MODEL_DIR,  map_location=lambda storage, loc: storage))
+# NET.load_state_dict(torch.load(MODEL_DIR,  map_location=lambda storage, loc: storage))
 NET.load_state_dict(torch.load(MODEL_DIR,  map_location='cpu'))
 if torch.cuda.is_available():
     NET.cuda()
 NET.eval()
-
-def infer_image_type(image_data):
-    """根据图片的内容推断图片的格式"""
-    if image_data[:8] == b'\x89PNG\r\n\x1a\n':
-        return ".png"
-    if image_data[:2] == b'\xff\xd8':
-        return ".jpg"
-    return ".jpg"
 
 def image_sketch(image_data):
     if isinstance(image_data, list):
@@ -69,13 +64,13 @@ def image_sketch(image_data):
         image_data_list = [image_data]
     img_name_list = []
     for image_data in image_data_list:
-        ext = infer_image_type(image_data[:10])
+        ext = imghdr.what(None, image_data)
         fn = "{}.{}".format(time.time(), ext)
         with open(fn, "wb") as fp:
             fp.write(image_data)
         img_name_list.append(fn)
 
-    result_list = []    
+    result_list = []
     test_salobj_dataset = SalObjDataset(img_name_list = img_name_list,
                                         lbl_name_list = [],
                                         transform=transforms.Compose([RescaleT(320), ToTensorLab(flag=0)])
@@ -93,11 +88,11 @@ def image_sketch(image_data):
         else:
             inputs_test = Variable(inputs_test)
 
-        d1,d2,d3,d4,d5,d6,d7 = NET(inputs_test)
+        d1, d2, d3, d4, d5, d6, d7 = NET(inputs_test)
         # normalization
-        pred = d1[:,0,:,:]
+        pred = d1[:, 0, :, :]
         pred = normPRED(pred)
-        dat = gen_output(img_name_list[i_test],pred)
+        dat = gen_output(img_name_list[i_test], pred)
         result_list.append(dat)
         del d1,d2,d3,d4,d5,d6,d7
 
