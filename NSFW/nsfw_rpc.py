484b67eb4a8be401d1b246315ed119b91916108a
linxiaohui <llinxiaohui@126.com>
2020-12-07 08:49:09 +0800

    NSFW bugfix

--- NSFW/nsfw_rpc.py
+++ NSFW/nsfw_rpc.py
@@ -8,12 +8,12 @@ from nsfw_predict import nsfw_predict
 from nsfw_predict import nsfw_batch_predict
 
 class ImageService(object):
-    def nsfw_predict(self, image_data, image_ext='jpg'):
-        ret = nsfw_predict(image_data, image_ext)
+    def nsfw_predict(self, image_data):
+        ret = nsfw_predict(image_data)
         print(ret)
         return ret
-    def nsfw_batch_predict(self, images, image_ext='jpg'):
-        ret = nsfw_batch_predict(images, image_ext)
+    def nsfw_batch_predict(self, images):
+        ret = nsfw_batch_predict(images)
         print(ret)
         return ret
 
