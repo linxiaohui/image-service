2e7b17dfcd028c9bcea2516ba52ecb72272c9214
linxiaohui <llinxiaohui@126.com>
2020-12-18 12:35:42 +0800

    fix onnx

--- BeautyPredict/fix_onnx.py
+++ BeautyPredict/fix_onnx.py
@@ -8,5 +8,3 @@ new_model_path = "beauty-predict-fix.onnx"
 # Save the ONNX model
 variable_length_model = update_model_dims.update_inputs_outputs_dims(onnx_model, {'resnet50_input': [1, 224, 224, 3]}, {'dense_1': [1, 5]})
 onnx.save(variable_length_model, new_model_path)
-
-~                                                      
